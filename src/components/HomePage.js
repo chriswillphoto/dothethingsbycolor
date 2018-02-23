a8a18d0d1614044b03641ab11b766b1b6f9b5961
chriswillphoto <christophernekich@gmail.com>
2018-02-23 15:10:16 +1100

    got rid of redux, dont need it for project this small, structuring

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React, { PureComponent as Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: []
+    };
+  }
+}
+
+export default App;
