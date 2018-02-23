28d484db530141ee068195b5d285b6443634cae8
chriswillphoto <christophernekich@gmail.com>
2018-02-23 16:49:11 +1100

    sorted priority list by level of priority in descending order

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,32 @@
 import React, { PureComponent as Component } from "react";
 import Priority from './Priority'
 
+
+const compare = function(a, b) {
+  const levelA = a.level
+  const levelB = b.level
+
+  let comparison = 0;
+  if (levelA < levelB) {
+    comparison = 1;
+  } else if (levelA > levelB) {
+    comparison = -1;
+  }
+  return comparison;
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: []
+      items: [{name: "one", level: 4}, {name: "two", level: 5}]
     };
   }
 
   listPriorities() {
-    this.state.items.map( priority => {
-      <Priority level={priority.level} />
+    return this.state.items.sort(compare).map( priority => {
+      return <Priority priority={priority} />
     } )
   }
 
