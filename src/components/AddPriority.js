15ab8466bfa1c0a44f5d36320b5e78c3b813fe05
chriswillphoto <christophernekich@gmail.com>
2018-02-24 13:34:26 +1100

    form input and select added to addpriority

--- src/components/AddPriority.js
+++ src/components/AddPriority.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const AddPriority = (props) => {
-  return(
-    <div className="add-form" >
-      <h1> add form will go here </h1>
-    </div>
-  )
-}
+const AddPriority = props => {
+  const conn = e => {
+    e.preventDefault();
+    console.log(e.target.elements[0].value);
+    console.log(e.target.elements[1].value);
+    const newPriority = {
+      name: e.target.elements[0].value,
+      level: e.target.elements[1].value
+    };
+    props.addToPriorities(newPriority);
+  };
 
-export default AddPriority
+  return (
+    <form
+      className="add-form"
+      onSubmit={e => {
+        conn(e);
+      }}
+    >
+      <input className="priority-name" placeholder="Name of Priority" />
+      <select>
+        <option value={5}>5 (Top Priority)</option>
+        <option value={4}>4 (Very Important)</option>
+        <option value={3}>3 (Has to be done, but can wait)</option>
+        <option value={2}>2 (Can be put of if need be)</option>
+        <option value={1}>1 (Only done if you can find the time)</option>
+      </select>
+      <button>Add</button>
+    </form>
+  );
+};
+
+export default AddPriority;
