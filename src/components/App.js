887b2c138b2427c6c74c46ac6ff5620479c254f7
chriswillphoto <christophernekich@gmail.com>
2018-02-26 19:30:12 +1100

    corrected import path

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React, { PureComponent as Component } from "react";
-import Priority from './Priority'
-import AddPriority from './AddPriority'
-
+import TransitionGroup from "react-transition-group/TransitionGroup";
+import Priority from "./Priority";
+import AddPriority from "./AddPriority";
 
 const compare = function(a, b) {
-  const levelA = a.level
-  const levelB = b.level
+  const levelA = a.level;
+  const levelB = b.level;
 
   let comparison = 0;
   if (levelA < levelB) {
@@ -14,36 +14,36 @@ const compare = function(a, b) {
     comparison = -1;
   }
   return comparison;
-}
+};
 
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      items: [{name: "one", level: 4}, {name: "two", level: 5}]
+      items: [{ name: "one", level: 4 }, { name: "two", level: 5 }]
     };
   }
 
   listPriorities() {
-    return this.state.items.sort(compare).map( priority => {
-      return <Priority priority={priority} />
-    } )
+    return this.state.items.sort(compare).map(priority => {
+      return <Priority priority={priority} />;
+    });
   }
 
   addToPriorites(p) {
-    const newListOfPriorites = [...this.state.items, p]
-    console.log(newListOfPriorites)
-    this.setState({items: newListOfPriorites})
+    const newListOfPriorites = [...this.state.items, p];
+    console.log(newListOfPriorites);
+    this.setState({ items: newListOfPriorites });
   }
 
   render() {
-    return(
-      <div className="priorities-container" >
+    return (
+      <div className="priorities-container">
         {this.listPriorities()}
-        <AddPriority addToPriorities={(p) => this.addToPriorites(p)}/>
+        <AddPriority addToPriorities={p => this.addToPriorites(p)} />
       </div>
-    )
+    );
   }
 }
 
